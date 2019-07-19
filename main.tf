a17ab9e9cb49049ff53468c669f7a7b9a7b55ebf
venkat1290 <50834267+venkat1290@users.noreply.github.com>
2019-07-19 14:48:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -351,7 +351,7 @@ resource "azurerm_virtual_machine_extension" "testextension" {
 
       settings = <<SETTINGS
       {
-	"commandToExecute": "yum install -y wget && yum install -y git && yum -y update && yum install -y java-1.8.0-openjdk && sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo && sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key && sudo yum install -y jenkins"
+	"commandToExecute": "yum install -y wget && yum install -y git && yum -y update && yum install -y java-1.8.0-openjdk && sudo wget /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo && sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key && sudo yum install -y jenkins"
       }
     SETTINGS
 
